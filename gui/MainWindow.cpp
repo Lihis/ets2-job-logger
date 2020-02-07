Lihis/ets2-job-logger

commit 92bd801a0d72d6e4e5c88b4af4a7ac8cde3f9232

Fixes #34, added trailer ID and trailer accessory ID

plugin/include/jobplugin/PluginDefs.h
@@ -156,6 +156,9 @@ struct job_t {
     explicit job_t(const Game &game = Game::Unknown) {
         this->game = game;
         status = JobStatus::FreeAsWind;
+#ifdef PLUGIN_INTERNAL
+        prevStatus = JobStatus::FreeAsWind;
+#endif
         type = JobType::Unknown;
         isSpecial = false;
         income = 0;
@@ -175,6 +178,9 @@ struct job_t {
 
     Game game;
     JobStatus status;
+#ifdef PLUGIN_INTERNAL
+    JobStatus prevStatus;
+#endif
     JobType type;
     bool isSpecial;
     uint64_t income;
@@ -197,12 +203,12 @@ struct job_t {
     } distance;
 
     struct trailer_t {
-        trailer_t() {
-            connected = false;
-        }
+        trailer_t() : id(), accessoryId() {}
 
-        bool connected;
-        MSGPACK_DEFINE(connected)
+        std::string id;
+        std::string accessoryId;
+
+        MSGPACK_DEFINE(id, accessoryId);
     } trailer;
 
     struct cargo_t {
@@ -265,6 +271,11 @@ struct job_t {
         cargoObj["mass"] = cargo.mass;
         cargoObj["damage"] = cargo.damage;
         root["cargo"] = cargoObj;
+
+        Json::Value trailerObj;
+        trailerObj["id"] = trailer.id;
+        trailerObj["accessoryId"] = trailer.accessoryId;
+        root["trailer"] = trailerObj;
 
         root["source"] = Json::Value();
         source.Serialize(root["source"], isSpecial);

plugin/src/Logger.cpp
@@ -95,10 +95,6 @@ SCSAPI_VOID Logger::truckPlacement(const scs_value_t *const placement) {
     m_truck.heading = orientation.heading;
 }
 
-SCSAPI_VOID Logger::trailerConnected(const scs_value_t *const connected) {
-    m_job.trailer.connected = connected ? connected->value_bool.value : false;
-}
-
 SCSAPI_VOID Logger::cargoDamage(const scs_value_t *const damage) {
     float prev = m_job.cargo.damage;
     float cur = damage ? damage->value_float.value : 0.f;
@@ -135,7 +131,18 @@ SCSAPI_VOID Logger::eventStarted(const void *const /*event_info*/) {
 SCSAPI_VOID Logger::configuration(const scs_telemetry_configuration_t *event_info) {
     std::string event_id(event_info->id);
 
-    if (event_id == SCS_TELEMETRY_CONFIG_job) {
+    if (event_id == SCS_TELEMETRY_CONFIG_trailer) {
+        for (auto attr = event_info->attributes; attr->name; attr++) {
+            LockGuard lock(m_lock);
+            std::string name(attr->name);
+
+            if (name == SCS_TELEMETRY_CONFIG_ATTRIBUTE_id) {
+                m_job.trailer.id = attr->value.value_string.value;
+            } else if (name == SCS_TELEMETRY_CONFIG_ATTRIBUTE_cargo_accessory_id) {
+                m_job.trailer.accessoryId = attr->value.value_string.value;
+            }
+        }
+    } else if (event_id == SCS_TELEMETRY_CONFIG_job) {
         uint8_t found = 0;
 
         for (auto attr = event_info->attributes; attr->name; attr++) {
@@ -202,9 +209,16 @@ SCSAPI_VOID Logger::configuration(const scs_telemetry_configuration_t *event_inf
 
         bool onJob = (found == (m_job.isSpecial ? 10 : 14));
         if (onJob && m_job.status == JobStatus::FreeAsWind) {
+            m_job.prevStatus = m_job.status;
             m_job.status = JobStatus::OnJob;
-            send_job();
         }
+    }
+
+    if (m_job.status == JobStatus::OnJob &&
+        m_job.status != m_job.prevStatus &&
+        !m_job.trailer.id.empty()) {
+        m_job.prevStatus = m_job.status;
+        send_job();
     }
 }
 
@@ -212,10 +226,12 @@ SCSAPI_VOID Logger::gameplay(const scs_telemetry_gameplay_event_t *event_info) {
     std::string event_id(event_info->id);
 
     if (event_id == SCS_TELEMETRY_GAMEPLAY_EVENT_job_cancelled) {
+        m_job.prevStatus = m_job.status;
         m_job.status = JobStatus::Cancelled;
     } else if (event_id == SCS_TELEMETRY_GAMEPLAY_EVENT_job_delivered) {
+        m_job.prevStatus = m_job.status;
         m_job.status = JobStatus::Delivered;
-    } else if (event_id != SCS_TELEMETRY_GAMEPLAY_EVENT_player_fined) {
+    } else {
         return;
     }
 

plugin/src/Logger.h
@@ -80,13 +80,6 @@ public:
     SCSAPI_VOID truckPlacement(const scs_value_t *const placement);
 
     /**
-     * Trailer connected callback
-     *
-     * @param connected
-     */
-    SCSAPI_VOID trailerConnected(const scs_value_t *const connected);
-
-    /**
      * Cargo damage callback
      *
      * Damage is not actually updated in job_t structure as it will be received

plugin/src/main.cpp
@@ -45,7 +45,6 @@ callback_channel_t odometer_cb;
 callback_channel_t fuel_cb;
 callback_channel_t speed_cb;
 callback_channel_t truckPlacement_cb;
-callback_channel_t trailerConnected_cb;
 callback_channel_t cargoDamage_cb;
 callback_event_t frameEnd_cb;
 callback_event_t eventPaused_cb;
@@ -86,7 +85,7 @@ SCSAPI_VOID callback_channel(const scs_string_t name, const scs_u32_t /*index*/,
     (*callback)(value);
 }
 
-SCSAPI_VOID callback_event(const scs_event_t event, const void *const event_info, scs_context_t context) {
+SCSAPI_VOID callback_event(const scs_event_t /*event*/, const void *const event_info, scs_context_t context) {
     if (!context) {
         return;
     }
@@ -132,9 +131,6 @@ SCSAPI_RESULT scs_telemetry_init(const scs_u32_t version, const scs_telemetry_in
 
     truckPlacement_cb = cb::bind::channel(&Logger::truckPlacement, logger);
     init_params->register_for_channel(SCS_TELEMETRY_TRUCK_CHANNEL_world_placement, SCS_U32_NIL, SCS_VALUE_TYPE_dplacement, SCS_TELEMETRY_CHANNEL_FLAG_no_value, callback_channel, (void *)&truckPlacement_cb);
-
-    trailerConnected_cb = cb::bind::channel(&Logger::trailerConnected, logger);
-    init_params->register_for_channel(SCS_TELEMETRY_TRAILER_CHANNEL_connected, SCS_U32_NIL, SCS_VALUE_TYPE_bool, SCS_TELEMETRY_CHANNEL_FLAG_no_value, callback_channel, (void *)&trailerConnected_cb);
 
     cargoDamage_cb = cb::bind::channel(&Logger::cargoDamage, logger);
     init_params->register_for_channel(SCS_TELEMETRY_JOB_CHANNEL_cargo_damage, SCS_U32_NIL, SCS_VALUE_TYPE_float, SCS_TELEMETRY_CHANNEL_FLAG_no_value, callback_channel, (void *)&cargoDamage_cb);

commit 663f06656ddae62df5a282a0e3bdc0d3c8c3e495

"Fixes" crashing on Windows introduced in 92bd801
Should prevent crashing but doesn't fix the underlying issue

gui/MainWindow.cpp
@@ -31,7 +31,7 @@ MainWindow::MainWindow(Settings *settings) :
 base::MainWindow(nullptr),
 m_sender(nullptr),
 m_settings(settings),
-m_job(Game::Unknown),
+m_job(),
 m_odometerOnStart(-1.f),
 m_fuelOnStart(-1.f),
 m_socket_running(false)
@@ -51,6 +51,10 @@ m_socket_running(false)
     Connect(wxEVT_CLOSE_WINDOW, wxCloseEventHandler(MainWindow::on_close));
     Connect(wxEVT_COMMAND_TEXT_UPDATED, wxCommandEventHandler(MainWindow::on_job_update));
     Bind(wxEVT_THREAD, &MainWindow::on_sender_update, this);
+
+    wxCommandEvent event(wxEVT_COMMAND_TEXT_UPDATED);
+    event.SetInt(static_cast<int>(PacketType::Job));
+    AddPendingEvent(event);
 }
 
 MainWindow::~MainWindow() {
@@ -67,8 +71,9 @@ void MainWindow::on_job_update(wxCommandEvent &event) {
     const auto type = static_cast<PacketType>(event.GetInt());
 
     if (type == PacketType::Job) {
-        m_lblCargo->SetLabel(wxString::FromUTF8(m_job.cargo.name.c_str()));
         if (m_job.status != JobStatus::FreeAsWind) {
+            m_lblCargo->SetLabel(wxString::FromUTF8(m_job.cargo.name.c_str()));
+
             wxString origin(wxString::FromUTF8(m_job.source.city.name.c_str()));
             wxString sender(wxString::FromUTF8(m_job.source.company.name.c_str()));
             m_lblOrigin->SetLabel(origin + (sender.empty() ? "" : " (" + sender + ")"));
@@ -77,6 +82,7 @@ void MainWindow::on_job_update(wxCommandEvent &event) {
             wxString receiver(wxString::FromUTF8(m_job.destination.company.name.c_str()));
             m_lblDestination->SetLabel(destination + (receiver.empty() ? "" : " (" + receiver + ")"));
         } else {
+            m_lblCargo->SetLabel("-");
             m_lblOrigin->SetLabel("-");
             m_lblDestination->SetLabel("-");
         }
@@ -214,17 +220,14 @@ void MainWindow::socket_run() {
 void MainWindow::socket_connect() {
     wxSleep(1);
     websocketpp::lib::error_code ec;
-    m_connection = m_client.get_connection("ws://127.0.0.1:" + std::to_string(WEBSOCK_PORT), ec);
+    std::string uri("ws://127.0.0.1:" + std::to_string(WEBSOCK_PORT));
+    WebsocketClient::connection_ptr con = m_client.get_connection(uri, ec);
     if (!ec) {
-        m_client.connect(m_connection);
+        m_client.connect(con);
     }
 }
 
 void MainWindow::socket_on_fail() {
-    m_job = job_t();
-    wxCommandEvent event(wxEVT_COMMAND_TEXT_UPDATED);
-    event.SetInt(static_cast<int>(PacketType::Job));
-    AddPendingEvent(event);
     socket_connect();
 }
 
@@ -252,7 +255,7 @@ void MainWindow::socket_on_message(const websocketpp::connection_hdl &hdl,
         }
 
         if (m_job.status == JobStatus::Cancelled || m_job.status == JobStatus::Delivered) {
-            m_job = job_t(m_job.game);
+            m_job = job_t();
             m_odometerOnStart = -1.f;
             m_fuelOnStart = -1.f;
         }

gui/MainWindow.h
@@ -133,7 +133,6 @@ private:
     std::mutex m_socket_lock;
     std::thread m_socket_thread;
     WebsocketClient m_client;
-    WebsocketClient::connection_ptr m_connection;
 };
 
 #endif //ETS2_JOB_LOGGER_MAINWINDOW_H

plugin/include/jobplugin/PluginDefs.h
@@ -68,9 +68,7 @@ struct version_t {
 };
 
 struct id_name_t {
-    id_name_t() {
-        id = "";
-        name = "";
+    id_name_t() : id(), name() {
     }
 
     std::string id;
@@ -212,11 +210,7 @@ struct job_t {
     } trailer;
 
     struct cargo_t {
-        cargo_t() {
-            id = "";
-            name = "-";
-            mass = 0.f;
-            damage = 0.f;
+        cargo_t() : id(), name(), mass(0.f), damage(0.f) {
         }
 
         std::string id;

plugin/src/Logger.cpp
@@ -301,7 +301,7 @@ void Logger::send(const std::string &data) {
     for (it = m_connections.begin(); it != m_connections.end(); ++it) {
         try {
             m_server.send(*it, data, websocketpp::frame::opcode::binary);
-        } catch (const std::exception &exception) {
+        } catch (const std::exception &/*exception*/) {
         }
     }
 }